2e9b099de79280e38d1a02fbc7bb79a6c25b416f
azra <azrayazidalkautsar@gmail.com>
2024-01-09 01:59:22 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,11 @@ AppDataSource.initialize().then(async () => {
     app.use(express.json())
     const corsOptions = {
         origin: '*',
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        methods: ["GET","PUT","PATCH","POST","DELETE"],
         preflightContinue: false,
         optionsSuccessStatus: 204
       }
-
+      
     app.use(cors(corsOptions))
 
     // router
